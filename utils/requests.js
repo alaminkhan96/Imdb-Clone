fa8737d466c725b6a66af774381573049863ff80
Al-Amin Khan <s.alaminkhan96@gmail.com>
2022-08-29 21:36:47 +0600

    Imdb clone app using the moviedb database

---
+++ utils/requests.js
@@ -0,0 +1,14 @@
+const API_KEY = process.env.API_KEY
+
+
+
+export default{
+    fetchTrending:{
+        title: "Trending",
+        url: `/trending/all/week?api_key=${API_KEY}&language=en-us`,
+    },
+    fetchTopRated:{
+        title: "Top Rated",
+        url: `/movie/all/top_rated?api_key=${API_KEY}&language=en-us`,
+    },
+}
